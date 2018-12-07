751f8a47593198a408a51a7cb1c4153166469c60
abhineet123 <asingh1@ualberta.ca>
2018-12-07 07:29:37 -0700

    minor fix

--- moveFilesToSubDirs.py
+++ moveFilesToSubDirs.py
@@ -2,33 +2,23 @@ import sys
 import os
 import random
 import shutil
+from Misc import processArguments
 
-subdir_prefix = 'Set'
-subdir_root_dir = '.'
-files_per_subdir = 100
-subdir_start_id = 1
-rename_files = 1
-shuffle_files = 1
-
-arg_id = 1
-if len(sys.argv) > arg_id:
-    subdir_prefix = sys.argv[arg_id]
-    arg_id += 1
-if len(sys.argv) > arg_id:
-    files_per_subdir = int(sys.argv[arg_id])
-    arg_id += 1
-if len(sys.argv) > arg_id:
-    subdir_start_id = int(sys.argv[arg_id])
-    arg_id += 1
-if len(sys.argv) > arg_id:
-    rename_files = int(sys.argv[arg_id])
-    arg_id += 1
-if len(sys.argv) > arg_id:
-    shuffle_files = int(sys.argv[arg_id])
-    arg_id += 1
-if len(sys.argv) > arg_id:
-    subdir_root_dir = sys.argv[arg_id]
-    arg_id += 1
+params = {
+    'subdir_prefix': 'Set',
+    'subdir_root_dir': '.',
+    'files_per_subdir': 100,
+    'subdir_start_id': 1,
+    'rename_files': 1,
+    'shuffle_files': 1,
+}
+processArguments(sys.argv[1:], params)
+subdir_prefix = params['subdir_prefix']
+subdir_root_dir = params['subdir_root_dir']
+files_per_subdir = params['files_per_subdir']
+subdir_start_id = params['subdir_start_id']
+rename_files = params['rename_files']
+shuffle_files = params['shuffle_files']
 
 print 'subdir_prefix: {:s}'.format(subdir_prefix)
 print 'files_per_subdir: {:d}'.format(files_per_subdir)
@@ -50,7 +40,7 @@ for src_fname in src_file_names:
         dst_fname = '{:s}{:d}_{:d}.{:s}'.format(subdir_prefix, subdir_id, file_id, file_extension)
     else:
         dst_fname = src_fname
-    dst_subdir_name = '{:s} {:d}'.format(subdir_prefix, subdir_id)
+    dst_subdir_name = '{:s}{:d}'.format(subdir_prefix, subdir_id)
     dst_dir = os.path.join(subdir_root_dir, dst_subdir_name)
     if not os.path.exists(dst_dir):
         os.makedirs(dst_dir)
---
+++ scripts/flifv.cmd
@@ -0,0 +1,2 @@
+@echo off
+python "%~dp0\..\filterLines.py" "filter_strings=.mkv,.avi,.mp4,.webm" filter_type=1 retain_filtered=%1 in_fname=%2 out_fname=%3
