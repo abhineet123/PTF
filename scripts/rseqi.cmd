f5690ec864fc5551def84a1a610498c81f20d552
abhineet123 <asingh1@ualberta.ca>
2019-01-13 18:32:17 -0700

    minor fix

--- scripts/rseqi.cmd
+++ scripts/rseqi.cmd
@@ -1,2 +1,2 @@
 @echo off
-python "%~dp0\..\renameFilesIntoSeq.py" image seq_prefix=1 seq_start_id=0 shuffle_files=1 seq_root_dir=%1 target_ext=%2 
+python "%~dp0\..\renameFilesIntoSeq.py" seq_prefix=image filename_fmt=1 seq_start_id=1 shuffle_files=0 seq_root_dir=%1 target_ext=%2 
