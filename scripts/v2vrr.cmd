b251dc35641c7a35b1b63805fa009b3ee7d67cab
abhineet123 <asingh1@ualberta.ca>
2019-03-17 10:46:04 -0600

    minor fix

--- scripts/v2vrr.cmd
+++ scripts/v2vrr.cmd
@@ -1,2 +1,2 @@
 @echo off
-python3 "%~dp0\..\encodeVideo.py" src_path=%1 fps=%2 res=%3 start_id=%4 n_frames=%5 save_path=%6 reverse=2 codec=FFV1 ext=avi out_postfix=rev
+python3 "%~dp0\..\encodeVideo.py" src_path=%1 fps=%2 res=%3 start_id=%4 n_frames=%5 save_path=%6 reverse=2 codec=FFV1 ext=avi
