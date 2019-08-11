2ad547f27ae8ffba812775376395f822ed1020ec
abhineet123 <asingh1@ualberta.ca>
2019-08-11 08:26:42 -0600

    Minor fixes in the DETRAC XML to CSV converter to get it working

--- xmlDETRACToMOT.py
+++ xmlDETRACToMOT.py
@@ -1,11 +1,11 @@
 import xml.etree.cElementTree as ET
-from .Misc import getParamDict
+from Misc import getParamDict
 import glob
 
 root_dir = 'E:/Datasets'
 actor_id = 4
 start_id = 0
-end_id = 2
+end_id = -1
 ignored_region_only = 0
 
 params = getParamDict()
@@ -13,6 +13,10 @@ actors = params['mot_actors']
 sequences = params['mot_sequences']
 actor = actors[actor_id]
 n_frames_list = []
+
+if end_id <= start_id:
+    end_id = len(sequences[actor]) - 1
+
 for seq_id in range(start_id, end_id + 1):
     seq_name = sequences[actor][seq_id]
     fname = '{:s}/{:s}/Annotations/{:s}.xml'.format(root_dir, actor, seq_name)
@@ -22,8 +26,10 @@ for seq_id in range(start_id, end_id + 1):
     out_fname = '{:s}/{:s}/Annotations/{:s}.txt'.format(root_dir, actor, seq_name)
     out_fid = open(out_fname, 'w')
 
-    for frame_obj in tree.iter('ignored_region'):
+    for obj in tree.iter('ignored_region'):
         bndbox = obj.find('box')
+        if bndbox is None:
+            continue
         xmin = float(bndbox.attrib['left'])
         ymin = float(bndbox.attrib['top'])
         width = float(bndbox.attrib['width'])
