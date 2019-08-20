3d3bfc3279e64cadcdb8d9af8d92a0da457bce2d
abhineet123 <asingh1@ualberta.ca>
2019-08-19 23:29:26 -0600

    minor fix

--- setBehindTopMost.py
+++ setBehindTopMost.py
@@ -3,7 +3,8 @@ import os
 import psutil
 import inspect
 import mouse
-import win32gui, win32con
+import sys
+import win32gui, win32con, win32com.client
 import numpy as np
 
 from Misc import processArguments
@@ -12,6 +13,7 @@ params = {
     'win_name': '.',
     'win_name2': '',
     'check_interval': 1000,
+    'win_names_fname': 'vwm_win_names.txt',
 }
 if __name__ == '__main__':
     p = psutil.Process(os.getpid())
@@ -24,6 +26,7 @@ if __name__ == '__main__':
     win_name = params['win_name']
     win_name2 = params['win_name2']
     check_interval = params['check_interval']
+    win_names_fname = params['win_names_fname']
 
     if not win_name or not win_name2:
         script_filename = inspect.getframeinfo(inspect.currentframe()).filename
@@ -32,11 +35,11 @@ if __name__ == '__main__':
         log_dir = os.path.join(script_path, 'log')
         if not os.path.isdir(log_dir):
             os.makedirs(log_dir)
-        win_names_file = os.path.join(log_dir, 'vwm_win_names.txt')
-        print('Reading vwm win_names from {}'.format(log_file))
+        win_names_file = os.path.join(log_dir, win_names_fname)
+        print('Reading vwm win_names from {}'.format(win_names_file))
 
         lines = open(win_names_file, 'r').readlines()
-        win_name, win_name2 = [line.strip() for line in lines]
+        win_name, win_name2 = [line.strip() for line in lines if line.strip()]
 
     print('win_name: {}'.format(win_name))
     print('win_name2: {}'.format(win_name2))
@@ -49,13 +52,15 @@ if __name__ == '__main__':
         [1920, -1080],
     ]
 
+    prev_active_win_name = None
+
     interrupt_wait = Event()
 
     def getMonitorID(_win_name):
         _win_handle = win32gui.FindWindow(None, _win_name)
-        rect = win32gui.GetWindowRect(_win_handle)
-        x = (rect[0] + rect[2]) / 2.0
-        y = (rect[1] + rect[3]) / 2.0
+        _rect = win32gui.GetWindowRect(_win_handle)
+        x = (_rect[0] + _rect[2]) / 2.0
+        y = (_rect[1] + _rect[3]) / 2.0
 
         _monitor_id = 0
         min_dist = np.inf
@@ -67,7 +72,7 @@ if __name__ == '__main__':
                 min_dist = dist
                 _monitor_id = curr_id
 
-        return _monitor_id, _win_handle
+        return _monitor_id, _win_handle, _rect
 
 
     def mouse_click_callback():
@@ -83,21 +88,39 @@ if __name__ == '__main__':
 
         active_win_name = win32gui.GetWindowText(win32gui.GetForegroundWindow())
         print('active_win_name: {}'.format(active_win_name))
-
-        if (prev_active_win_name is None or prev_active_win_name != active_win_name) and active_win_name not in (
+        # (prev_active_win_name is None or prev_active_win_name != active_win_name)
+        if active_win_name not in (
                 win_name, win_name2):
             prev_active_win_name = active_win_name
 
-            _monitor_id, _win_handle =  getMonitorID(active_win_name)
+            _monitor_id, _win_handle, rect =  getMonitorID(active_win_name)
 
-            monitor_id, win_handle = getMonitorID(win_name)
-            monitor_id2, win_handle2 = getMonitorID(win_name2)
+            monitor_id, win_handle, _ = getMonitorID(win_name)
+            monitor_id2, win_handle2, _ = getMonitorID(win_name2)
+
+            print('_monitor_id: {}'.format(_monitor_id))
+            print('monitor_id: {}'.format(monitor_id))
+            print('monitor_id2: {}'.format(monitor_id2))
+
+            print('_win_handle: {}'.format(_win_handle))
+            print('win_handle: {}'.format(win_handle))
+            print('win_handle2: {}'.format(win_handle2))
 
             print('active_win_name: {} with pos: {} on monitor {}'.format(active_win_name, rect, _monitor_id))
 
             if _monitor_id == monitor_id:
+                # shell = win32com.client.Dispatch("WScript.Shell")
+                # shell.SendKeys('%')
+                win32gui.ShowWindow(win_handle, 5)
                 win32gui.SetForegroundWindow(win_handle)
+
+                win32gui.ShowWindow(_win_handle, 5)
                 win32gui.SetForegroundWindow(_win_handle)
             elif _monitor_id == monitor_id2:
+                # shell = win32com.client.Dispatch("WScript.Shell")
+                # shell.SendKeys('%')
+                win32gui.ShowWindow(win_handle2, 5)
                 win32gui.SetForegroundWindow(win_handle2)
+
+                win32gui.ShowWindow(_win_handle, 5)
                 win32gui.SetForegroundWindow(_win_handle)
