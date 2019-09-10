d3b4cdb0ee14fb643efbd9cf6caae7484b2729e0
abhineet123 <asingh1@ualberta.ca>
2019-09-09 19:01:37 -0600

    minor fix

--- scripts/wgetp.sh
+++ scripts/wgetp.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-python3 ~/PTF/wgetByParts.py url=$1 n_parts=$2 part_size=$3 size=$4 out_name=$5
+python3 ~/PTF/wgetByParts.py url=$1 n_parts=$2 start_id=$3 part_size=$4 size=$5 out_name=$6
