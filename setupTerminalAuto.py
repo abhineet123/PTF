ff31f2e26562fd4b6ab844f3ea99a7e6c0de6e0d
abhineet123 <asingh1@ualberta.ca>
2019-10-03 12:44:17 -0600

    minor fix

--- setupTerminalAuto.py
+++ setupTerminalAuto.py
@@ -15,7 +15,18 @@ if __name__ == '__main__':
     config = params['config']
     wait_t = params['wait_t']
 
-    auth_data = open('')
+    auth_data = open('pwd.txt', 'r').readlines()
+    auth_data = [k.strip() for k in auth_data]
+
+    name00, name01, pwd0 = auth_data[0].split(' ')
+    name10, name11, pwd1 = auth_data[1].split(' ')
+    name20, name21, pwd2 = auth_data[2].split(' ')
+
+    print('Setting up system 0 with tmux sessions {}, {} and pwd {}'.format(name00, name01, pwd0))
+    print('Setting up system 1 with tmux sessions {}, {} and pwd {}'.format(name10, name11, pwd1))
+    print('Setting up system 2 with tmux sessions {}, {} and pwd {}'.format(name20, name21, pwd2))
+
+    # sys.exit()
 
     if not exe_path:
         raise IOError('Terminal executable path must be provided')
@@ -37,7 +48,8 @@ if __name__ == '__main__':
             _app.fatty.type_keys("^+t")
             _app.fatty.type_keys("sstg{VK_SPACE}tb~")
             _app.fatty.type_keys("sudo{VK_SPACE}-s~")
-            _app.fatty.type_keys("';';';';~")
+            _app.fatty.type_keys("%s~" % pwd0)
+
 
             # time.sleep(1)
 
@@ -47,20 +59,20 @@ if __name__ == '__main__':
 
         time.sleep(5)
 
-        app.fatty.type_keys("tmux{VK_SPACE}a{VK_SPACE}-t{VK_SPACE}grs~")
-        app2.fatty.type_keys("tmux{VK_SPACE}a{VK_SPACE}-t{VK_SPACE}grs2~")
+        app.fatty.type_keys("tmux{VK_SPACE}a{VK_SPACE}-t{VK_SPACE}%s~" % name00)
+        app2.fatty.type_keys("tmux{VK_SPACE}a{VK_SPACE}-t{VK_SPACE}%s~" % name01)
 
         for _app in apps:
             _app.fatty.type_keys("^+t")
             _app.fatty.type_keys("sstg2~")
             _app.fatty.type_keys("sstz~")
             _app.fatty.type_keys("sudo{VK_SPACE}-s~")
-            _app.fatty.type_keys("'''~")
+            _app.fatty.type_keys("%s~" % pwd1)
 
         time.sleep(5)
 
-        app.fatty.type_keys("tmux{VK_SPACE}a{VK_SPACE}-t{VK_SPACE}orca~")
-        app2.fatty.type_keys("tmux{VK_SPACE}a{VK_SPACE}-t{VK_SPACE}orca2~")
+        app.fatty.type_keys("tmux{VK_SPACE}a{VK_SPACE}-t{VK_SPACE}%s~" % name10)
+        app2.fatty.type_keys("tmux{VK_SPACE}a{VK_SPACE}-t{VK_SPACE}%s~" % name11)
 
         # time.sleep(1)
 
@@ -73,10 +85,12 @@ if __name__ == '__main__':
             _app.fatty.type_keys("sstg3~")
             _app.fatty.type_keys("sstx~")
             _app.fatty.type_keys("sudo{VK_SPACE}-s~")
-            _app.fatty.type_keys("'''~")
+            _app.fatty.type_keys("%s~" % pwd2)
 
-        app.fatty.type_keys("tmux{VK_SPACE}a{VK_SPACE}-t{VK_SPACE}x99~")
-        app2.fatty.type_keys("tmux{VK_SPACE}a{VK_SPACE}-t{VK_SPACE}x992~")
+        time.sleep(5)
+
+        app.fatty.type_keys("tmux{VK_SPACE}a{VK_SPACE}-t{VK_SPACE}%s~" % name20)
+        app2.fatty.type_keys("tmux{VK_SPACE}a{VK_SPACE}-t{VK_SPACE}%s~" % name21)
 
         app3.fatty.type_keys("^b^r")
 
@@ -91,7 +105,7 @@ if __name__ == '__main__':
         app.fatty.type_keys("^+t")
         app.fatty.type_keys("sstg{VK_SPACE}tb~")
         app.fatty.type_keys("sudo{VK_SPACE}-s~")
-        app.fatty.type_keys("';';';';~")
+        app.fatty.type_keys("%s~" % pwd0)
         time.sleep(2)
 
         if config == 0:
@@ -103,7 +117,7 @@ if __name__ == '__main__':
         app.fatty.type_keys("sstg2~")
         app.fatty.type_keys("sstz~")
         app.fatty.type_keys("sudo{VK_SPACE}-s~")
-        app.fatty.type_keys("'''~")
+        app.fatty.type_keys("%s~" % pwd1)
 
         time.sleep(wait_t)
 
