07114016b3ded1eb5c7d5881b8beae6c867c5897
abhineet123 <asingh1@ualberta.ca>
2019-11-28 18:35:37 -0700

    Updated the multiple motion visualizer with automatic command script processing

--- vwm_multi.py
+++ vwm_multi.py
@@ -3,6 +3,7 @@ from multiprocessing import Process
 import os
 import time
 import random
+import shlex
 import win32api
 import win32gui, win32con
 
@@ -12,39 +13,63 @@ if __name__ == '__main__':
 
     prob = 0.5
     sleep = 60
-    frg_win_titles = 'The Journal 8,f,t,XYplorer 20.10'
+    script_root = 'scripts'
+    script_1 = 'vw32ntjv.cmd'
+    script_2 = 'vw32ntj.cmd'
 
-    args1 = 'on_top=0 top_border=0 keep_borders=1 n_images=1 random_mode=1 auto_progress=1 fps=15 monitor_id=0 ' \
-            'duplicate_window=0 second_from_top=1 win_offset_y=0 width=1920 height=1050 ' \
-            'src_dirs=vids/20/2/14,vids/20/2/13 ' \
-            'only_maximized=0 ' \
-            'reversed_pos=2 video_mode=2 multi_mode=1 auto_progress_video=1 preserve_order=1 lazy_video_load=0'
-    args2 = 'on_top=0 top_border=0 keep_borders=1 n_images=2 ' \
-            'random_mode=1 auto_progress=1 transition_interval=30 monitor_id=0  ' \
-            'duplicate_window=0 second_from_top=1 win_offset_y=0 width=1920 height=1050 ' \
-            'src_dirs=vids/20/2/13**10,vids/20/2_patches**10,!20/9,!20/10,20,20/2*5,20/1*3,20/1/1_0*5,20/1/1_1*25,' \
-            '20/1/1_8*8,20/1/1_9*20,20/1/1_3*4,20/1/1_5*15,20/1/1_2*15,20/1/1_4*20 ' \
-            'only_maximized=0 reversed_pos=2'
+    curr_path = os.path.dirname(os.path.abspath(__file__))
+
+    script_1_path = os.path.join(curr_path, script_root, script_1)
+    script_2_path = os.path.join(curr_path, script_root, script_2)
+
+    args1_lines = open(script_1_path, 'r').readlines()
+    args1 = [k for k in args1_lines if k.startswith('python3 ')]
+    assert len(args1) == 1, "Invalid script: {} with contents: {}".format(script_1_path, args1_lines)
+    args1 = args1[0]
+
+    args2_lines = open(script_2_path, 'r').readlines()
+    args2 = [k for k in args2_lines if k.startswith('python3 ')]
+    assert len(args2) == 1, "Invalid script: {} with contents: {}".format(script_2_path, args2_lines)
+    args2 = args2[0]
+
+
+    # frg_win_titles = 'The Journal 8,f,t,XYplorer 20.10'
+
+    # args1 = 'on_top=0 top_border=0 keep_borders=1 n_images=1 random_mode=1 auto_progress=1 fps=15 monitor_id=0 ' \
+    #         'duplicate_window=0 second_from_top=1 win_offset_y=0 width=1920 height=1050 ' \
+    #         'src_dirs=vids/20/2/14,vids/20/2/13 ' \
+    #         'only_maximized=0 ' \
+    #         'reversed_pos=2 video_mode=2 multi_mode=1 auto_progress_video=1 preserve_order=1 lazy_video_load=0'
+    # args2 = 'on_top=0 top_border=0 keep_borders=1 n_images=2 ' \
+    #         'random_mode=1 auto_progress=1 transition_interval=30 monitor_id=0  ' \
+    #         'duplicate_window=0 second_from_top=1 win_offset_y=0 width=1920 height=1050 ' \
+    #         'src_dirs=vids/20/2/13**10,vids/20/2_patches**10,!20/9,!20/10,20,20/2*5,20/1*3,20/1/1_0*5,20/1/1_1*25,' \
+    #         '20/1/1_8*8,20/1/1_9*20,20/1/1_3*4,20/1/1_5*15,20/1/1_2*15,20/1/1_4*20 ' \
+    #         'only_maximized=0 reversed_pos=2'
 
     time_stamp = datetime.now().strftime("%y%m%d_%H%M%S")
 
-    args1 = [k.strip() for k in args1.split(' ') if k.strip()]
     win_name1 = 'VWM_multi_vid_{}'.format(os.path.basename(time_stamp))
     win_name2 = 'VWM_multi_img_{}'.format(os.path.basename(time_stamp))
 
+    # args1 = [k.strip() for k in args1.split(' ') if k.strip()]
+    args1 = shlex.split(args1)
+    args1 = [k for k in args1[2:] if '%' not in k]
     args1.append('win_name={}'.format(win_name1))
-    args1.append('frg_win_titles={}'.format(frg_win_titles))
+    # args1.append('frg_win_titles={}'.format(frg_win_titles))
     args1.append('nazio_win_name={}'.format(win_name2))
 
-    args2 = [k.strip() for k in args2.split(' ') if k.strip()]
+    # args2 = [k.strip() for k in args2.split(' ') if k.strip()]
+    args2 = shlex.split(args2)
+    args2 = [k for k in args2[2:] if '%' not in k]
     args2.append('win_name={}'.format(win_name2))
-    args2.append('frg_win_titles={}'.format(frg_win_titles))
+    # args2.append('frg_win_titles={}'.format(frg_win_titles))
     args2.append('nazio_win_name={}'.format(win_name1))
 
-    vwm1_thread = Process(target=vwm.main, args=(args1, ))
+    vwm1_thread = Process(target=vwm.main, args=(args1[2:],))
     vwm1_thread.start()
 
-    vwm2_thread = Process(target=vwm.main, args=(args2, ))
+    vwm2_thread = Process(target=vwm.main, args=(args2[2:],))
     vwm2_thread.start()
 
     time.sleep(5)
@@ -75,4 +100,3 @@ if __name__ == '__main__':
                     hidden_win_handle = _win_handle_1
         except:
             break
-
