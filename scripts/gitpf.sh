b9989387a4a25f149009cc63e5b0d6424bd8c74f
abhineet123 <asingh1@ualberta.ca>
2019-12-14 12:02:25 -0700

    minor fix

---
+++ scripts/gitpf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -v
+
+git fetch --all
+git reset --hard origin/master
+
--- scripts/vw32ntjv2.cmd
+++ scripts/vw32ntjv2.cmd
@@ -1,3 +1,3 @@
 @echo off
-python3 "%~dp0\..\vwm_multi.py"
+python3 "%~dp0\..\vwm_multi.py" prob=0.2 sleep_1=60 sleep_2=60 start_sleep=150
 
