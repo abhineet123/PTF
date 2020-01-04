c785341b8e70e4b02b9f76b1d151530525638dcd
abhineet123 <asingh1@ualberta.ca>
2020-01-03 18:50:32 -0700

    minor fix

--- scripts/i2it2.cmd
+++ scripts/i2it2.cmd
@@ -1,2 +1,2 @@
 @echo off
-python3 "%~dp0\..\videoToImgSeq.py" tracker_type=1 crop=2 mode=1 show_img=1 seq_name=%1 n_frames=%2 start_id=%3 dst_dir=%4 reverse=%5
+python3 "%~dp0\..\videoToImgSeq.py" tracker_type=1 crop=2 mode=1 show_img=1 seq_name=%1 tracker_type=%2 start_id=%3 dst_dir=%4 reverse=%5
