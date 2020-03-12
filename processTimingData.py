5f40865a8275236a274dee77becde33827856d39
abhineet123 <asingh1@ualberta.ca>
2020-03-11 20:33:38 -0600

    minor fix

--- processTimingData.py
+++ processTimingData.py
@@ -9,27 +9,72 @@ except ImportError:
 import time
 import win32gui, win32api
 from pywinauto import application, mouse
+import ctypes
 
+EnumWindows = ctypes.windll.user32.EnumWindows
+EnumWindowsProc = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int))
+GetWindowText = ctypes.windll.user32.GetWindowTextW
+GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
+IsWindowVisible = ctypes.windll.user32.IsWindowVisible
+
+titles = []
+
+
+def foreach_window(hwnd, lParam):
+    if IsWindowVisible(hwnd):
+        length = GetWindowTextLength(hwnd)
+        buff = ctypes.create_unicode_buffer(length + 1)
+        GetWindowText(hwnd, buff, length + 1)
+        titles.append((hwnd, buff.value))
+    return True
+
+
+win_titles = ['Timing', 'Google Chrome']
 try:
     in_txt = Tk().clipboard_get()
-except:
+except BaseException as e:
+    print('Tk().clipboard_get() failed: {}'.format(e))
     lines = []
 else:
     lines = in_txt.split('\n')
     lines = [line for line in lines if line.strip()]
 
-if all(line.endswith('.ogg') for line in lines):
+if lines and all(line.endswith('.ogg') for line in lines):
     out_lines = [line.split('-')[1].split('.')[0].replace('_', ':') for line in lines]
     out_txt = '\n'.join(sorted(out_lines))
 else:
-
-    time.sleep(1)
-
+    # time.sleep(1)
     try:
-        active_handle = win32gui.GetForegroundWindow()
-        target_title = win32gui.GetWindowText(active_handle)
+        orig_x, orig_y = win32api.GetCursorPos()
+        print('GetCursorPos x: {}'.format(orig_x))
+        print('GetCursorPos y: {}'.format(orig_y))
+
+        win32gui.EnumWindows(foreach_window, None)
+
+        # for i in range(len(titles)):
+        #     print(titles[i])
+
+        target_title = [k[1] for k in titles if all(title in k[1] for title in win_titles)]
+        # print('target_title: {}'.format(target_title))
+
+        if not target_title:
+            raise IOError('Window with win_titles: {} not found'.format(win_titles))
+
+        target_title = target_title[0]
+
+        target_handle = win32gui.FindWindow(None, target_title)
+        rect = win32gui.GetWindowRect(target_handle)
+
+        x = int((rect[0] + rect[2]) / 2)
+        y = int((rect[1] + rect[3]) / 2)
+
+        # active_handle = win32gui.GetForegroundWindow()
+        # target_title = win32gui.GetWindowText(active_handle)
 
         print('target_title: {}'.format(target_title))
+        print('rect: {}'.format(rect))
+        print('x: {}'.format(x))
+        print('y: {}'.format(y))
 
         try:
             app = application.Application().connect(title=target_title, found_index=0)
@@ -44,10 +89,13 @@ else:
         app_win.type_keys("^a")
         app_win.type_keys("^c")
 
-        x, y = win32api.GetCursorPos()
+        mouse.move(coords=(x, y))
 
         mouse.click(button='left', coords=(x, y))
         mouse.click(button='left', coords=(x, y))
+
+        mouse.move(coords=(orig_x, orig_y))
+
     except BaseException as e:
         print('BaseException: {}'.format(e))
 
