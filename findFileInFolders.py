b509766bfeb3a82b1fceb10f5f0b2a6e295a06c6
abhineet123 <asingh1@ualberta.ca>
2020-04-04 23:29:11 -0600

    added unique filenames finding functionality in the file finding script along with several more fixes therein including filename exclusions and extension inclusions and proper formatting of the printed unique names as well as matching names

--- findFileInFolders.py
+++ findFileInFolders.py
@@ -1,11 +1,14 @@
 import os
 import sys
+from pprint import pformat
+from Misc import sortKey
 
 root_dir = '.'
 folder_prefix = ''
 folder_start_id = 1
 folder_end_id = 100
 search_str = ''
+find_unique_names = 1
 
 arg_id = 1
 if len(sys.argv) > arg_id:
@@ -20,32 +23,76 @@ if len(sys.argv) > arg_id:
 if len(sys.argv) > arg_id:
     folder_prefix = sys.argv[arg_id]
     arg_id += 1
+if len(sys.argv) > arg_id:
+    find_unique_names = int(sys.argv[arg_id])
+    arg_id += 1
+
+exclusions = ['Thumbs.db', 'sync.ffs_db']
+img_exts = ('.jpg', '.bmp', '.jpeg', '.png', '.tif', '.tiff')
 
 if folder_prefix:
-    print 'Looking for {:s} in folders beginning with {:s} and IDs going from {:d} to {:d}'.format(
-        search_str, folder_prefix, folder_start_id, folder_end_id)
+    print('Looking for {:s} in folders beginning with {:s} and IDs going from {:d} to {:d}'.format(
+        search_str, folder_prefix, folder_start_id, folder_end_id))
     src_folders = ['{:s} {:d}'.format(folder_prefix, folder_id) for folder_id in
-                   xrange(folder_start_id, folder_end_id + 1)]
+                   range(folder_start_id, folder_end_id + 1)]
 else:
     src_folders = [os.path.join(root_dir, k) for k in os.listdir(root_dir)
-                    if os.path.isdir(os.path.join(root_dir, k))]
+                   if os.path.isdir(os.path.join(root_dir, k))]
+
+src_folders = sorted(src_folders, key=sortKey)
 
 total_files_found = 0
 total_files_searched = 0
+total_unique_names = 0
+
+matching_files = {}
 for src_folder in src_folders:
     if not os.path.exists(src_folder):
         break
-    src_files = [f for f in os.listdir(src_folder) if os.path.isfile(os.path.join(src_folder, f))]
+    src_files = [f for f in os.listdir(src_folder) if f not in exclusions and
+                 os.path.isfile(os.path.join(src_folder, f))]
+
     search_results = [f for f in src_files if search_str in f]
     if len(search_results) > 0:
-        print 'Found {:d} matching files in {:s}'.format(len(search_results), src_folder)
-        print '\n'.join(search_results)
+        print('Found {:d} matching files in {:s}'.format(len(search_results), src_folder))
+        print('\n'.join(search_results)+'\n')
         total_files_found += len(search_results)
+
+        matching_files[src_folder] = search_results
     else:
-        print 'Done searching {:s}'.format(src_folder)
+        print('Done searching {:s}'.format(src_folder))
+
+    unique_names = []
+    if find_unique_names:
+        src_files = [os.path.splitext(f)[0] for f in src_files if os.path.splitext(f)[1] in img_exts]
+        if src_files:
+            unique_names.append(src_files[0])
+        for i in range(1, len(src_files)):
+            commonprefix = os.path.commonprefix(src_files[i - 1:i + 1])
+            if not commonprefix:
+                unique_names.append(src_files[i])
+                continue
+
+            non_prefix = src_files[i].replace(commonprefix, '')
+            found_digit = 0
+            for _c in non_prefix:
+                if str(_c).isdigit():
+                    found_digit = 1
+                    break
+                if str(_c).isalpha():
+                    unique_names.append(src_files[i])
+                    break
+    if unique_names:
+        print('Found {} unique names:\n{}\n'.format(len(unique_names), unique_names))
+        total_unique_names += len(unique_names)
+
     total_files_searched += len(src_files)
+
+print('{:d} files searched'.format(total_files_searched))
+if find_unique_names:
+    print('{:d} unique names found'.format(total_unique_names))
+
 if total_files_found > 0:
-    print 'Total matching files found : {:d}'.format(total_files_found)
+    print('{} matching files found:\n{}'.format(total_files_found, pformat(matching_files)))
 else:
-    print 'No matching files found'
-print '{:d} files searched'.format(total_files_searched)
+    print('No matching files found')
---
+++ scripts/mp3tom4b.cmd
@@ -0,0 +1,2 @@
+@echo off
+python3 "%~dp0\..\mp3ToM4B.py" src_path=%1
