a3401297ae7c074ee508d4d0e5a3efbbf9bcb148
abhineet123 <asingh1@ualberta.ca>
2020-05-19 08:30:07 -0600

    minor fix

--- scp_from.py
+++ scp_from.py
@@ -19,25 +19,26 @@ if __name__ == '__main__':
 
     src_fname_no_ext, src_fname_ext = os.path.splitext(os.path.basename(src_fname))
 
-    if not src_fname_ext and not src_fname.endswith('/'):
-        """no ext --> directory -> add terminating / to prevent recreation of directory structure on dst"""
-        src_fname += '/'
-        src_fname_rel += '/'
+
 
     home_path = os.path.abspath(expanduser("~"))
     if src_fname_abs.startswith(home_path):
         src_fname_rel = os.path.relpath(src_fname, home_path)
         scp_fname = os.path.join('~', src_fname_rel)
     else:
+        src_fname_rel = src_fname
         scp_fname = src_fname
+
+    if not src_fname_ext and not src_fname.endswith('/'):
+        """no ext --> directory -> add terminating / to prevent recreation of directory structure on dst"""
+        src_fname += '/'
+        scp_fname += '/'
 
     print('src_fname_abs: {}'.format(src_fname_abs))
     print('src_fname: {}'.format(src_fname))
     print('home_path: {}'.format(home_path))
     print('src_fname_rel: {}'.format(src_fname_rel))
     print('scp_fname: {}'.format(scp_fname))
-
-
 
     src_dir = os.path.dirname(src_fname)
     if not os.path.isdir(src_dir):
