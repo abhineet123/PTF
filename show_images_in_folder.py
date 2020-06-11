af2da2610ef80a9cd6347f392191e706538084f9
abhineet123 <asingh1@ualberta.ca>
2020-06-11 10:12:11 -0600

    got free space computation working in SIIF

--- show_images_in_folder.py
+++ show_images_in_folder.py
@@ -1,121 +1,152 @@
 import cv2
 import sys
 import time
+import ctypes
+import platform
 import os, shutil
 from datetime import datetime
 from pprint import pformat
 
 from Misc import processArguments, sortKey, resizeAR
 
-params = {
-    'src_path': '.',
-    'save_path': '',
-    'save_root_dir': '',
-    'img_ext': 'jpg',
-    'show_img': 1,
-    'del_src': 0,
-    'start_id': 0,
-    'n_frames': 0,
-    'width': 0,
-    'height': 0,
-    'fps': 30,
-    # 'codec': 'FFV1',
-    # 'ext': 'avi',
-    'codec': 'H264',
-    'ext': 'mkv',
-    'out_postfix': '',
-    'reverse': 0,
-}
 
-processArguments(sys.argv[1:], params)
-src_path = params['src_path']
-save_path = params['save_path']
-img_ext = params['img_ext']
-show_img = params['show_img']
-del_src = params['del_src']
-start_id = params['start_id']
-n_frames = params['n_frames']
-_width = params['width']
-_height = params['height']
-fps = params['fps']
-codec = params['codec']
-ext = params['ext']
-out_postfix = params['out_postfix']
-reverse = params['reverse']
-save_root_dir = params['save_root_dir']
-
-img_exts = ('.jpg', '.bmp', '.jpeg', '.png', '.tif', '.tiff', '.webp')
-
-existing_images = {}
-_pause = 0
-
-src_path = os.path.abspath(src_path)
-script_dir = os.path.dirname(os.path.realpath(__file__))
-log_path =  os.path.join(script_dir, 'siif_log.txt')
-with open(log_path, 'w') as fid:
-    fid.write(src_path)
-
-os.environ["SIIF_DUMP_IMAGE_PATH"] = src_path
-
-read_img_path = os.path.join(src_path, "read")
-
-if os.path.exists(read_img_path):
-    shutil.rmtree(read_img_path)
-
-os.makedirs(read_img_path)
-
-print('SIIF started in {}'.format(src_path))
-
-while True:
-    _src_files = [k for k in os.listdir(src_path) if
-                  os.path.splitext(k.lower())[1] in img_exts]
-    for _src_file in _src_files:
-        _src_path = os.path.join(src_path, _src_file)
-        # mod_time = os.path.getmtime(_src_path)
-        _src_file_no_ext = os.path.splitext(_src_file)[0]
+def clear_dir(dirname):
+    for filename in os.listdir(dirname):
+        file_path = os.path.join(dirname, filename)
         try:
-            _src_file_id, _src_file_timestamp = _src_file_no_ext.split('___')
-        except ValueError as e:
-            print('Invalid _src_file: {} with _src_file_no_ext: {}'.format(
-                _src_file, _src_file_no_ext))
-
-        if _src_file_id not in existing_images or existing_images[_src_file_id] != _src_file_timestamp:
-            existing_images[_src_file] = _src_file_timestamp
-            print('reading {} with time: {}'.format(_src_file_id, _src_file_timestamp))
-
-            img = cv2.imread(_src_path)
-
-            # out_path = _src_path+'.done'
-            # print('writing to {}'.format(out_path))
-            # with open(out_path, 'w') as fid:
-            #     fid.write('siif')
-            #     fid.close()
-
-            _dst_path = os.path.join(read_img_path, os.path.basename(_src_path))
-
-            # os.remove(_src_path)
-
-            shutil.move(_src_path, _dst_path)
-            cv2.imshow(_src_file_id, img)
-
-        del_images = []
-        for existing_image in existing_images.keys():
-            if existing_image not in _src_files:
-                cv2.destroyWindow(existing_image)
-                del_images.append(existing_image)
-
-        for del_image in del_images:
-            del existing_images[del_image]
-
-    k = cv2.waitKey(1 - _pause)
-    if k == 27:
-        break
-    elif k == 32:
-        _pause = 1 - _pause
-
-for existing_image in existing_images.keys():
-    cv2.destroyWindow(existing_image)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 
+def get_free_space_mb(dirname):
+    """Return folder/drive free space (in megabytes)."""
+    if platform.system() == 'Windows':
+        free_bytes = ctypes.c_ulonglong(0)
+        ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(dirname), None, None, ctypes.pointer(free_bytes))
+        return free_bytes.value / 1024 / 1024
+    else:
+        st = os.statvfs(dirname)
+        return st.f_bavail * st.f_frsize / 1024 / 1024
 
 
+def main():
+    params = {
+        'src_path': '.',
+        'save_path': '',
+        'save_root_dir': '',
+        'img_ext': 'jpg',
+        'show_img': 1,
+        'del_src': 0,
+        'start_id': 0,
+        'n_frames': 0,
+        'width': 0,
+        'height': 0,
+        'fps': 30,
+        # 'codec': 'FFV1',
+        # 'ext': 'avi',
+        'codec': 'H264',
+        'ext': 'mkv',
+        'out_postfix': '',
+        'reverse': 0,
+    }
+
+    processArguments(sys.argv[1:], params)
+    src_path = params['src_path']
+    save_path = params['save_path']
+    img_ext = params['img_ext']
+    show_img = params['show_img']
+    del_src = params['del_src']
+    start_id = params['start_id']
+    n_frames = params['n_frames']
+    _width = params['width']
+    _height = params['height']
+    fps = params['fps']
+    codec = params['codec']
+    ext = params['ext']
+    out_postfix = params['out_postfix']
+    reverse = params['reverse']
+    save_root_dir = params['save_root_dir']
+
+    img_exts = ('.jpg', '.bmp', '.jpeg', '.png', '.tif', '.tiff', '.webp')
+
+    existing_images = {}
+    _pause = 0
+
+    src_path = os.path.abspath(src_path)
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    log_path = os.path.join(script_dir, 'siif_log.txt')
+    with open(log_path, 'w') as fid:
+        fid.write(src_path)
+
+    os.environ["SIIF_DUMP_IMAGE_PATH"] = src_path
+
+    read_img_path = os.path.join(src_path, "read")
+
+    if os.path.exists(read_img_path):
+        clear_dir(read_img_path)
+        # shutil.rmtree(read_img_path)
+    else:
+        os.makedirs(read_img_path)
+
+    print('SIIF started in {}'.format(src_path))
+
+    while True:
+        _src_files = [k for k in os.listdir(src_path) if
+                      os.path.splitext(k.lower())[1] in img_exts]
+        for _src_file in _src_files:
+            _src_path = os.path.join(src_path, _src_file)
+            # mod_time = os.path.getmtime(_src_path)
+            _src_file_no_ext = os.path.splitext(_src_file)[0]
+            try:
+                _src_file_id, _src_file_timestamp = _src_file_no_ext.split('___')
+            except ValueError as e:
+                print('Invalid _src_file: {} with _src_file_no_ext: {}'.format(
+                    _src_file, _src_file_no_ext))
+
+            if _src_file_id not in existing_images or existing_images[_src_file_id] != _src_file_timestamp:
+                existing_images[_src_file] = _src_file_timestamp
+                # print('reading {} with time: {}'.format(_src_file_id, _src_file_timestamp))
+
+                img = cv2.imread(_src_path)
+
+                # out_path = _src_path+'.done'
+                # print('writing to {}'.format(out_path))
+                # with open(out_path, 'w') as fid:
+                #     fid.write('siif')
+                #     fid.close()
+
+                _dst_path = os.path.join(read_img_path, os.path.basename(_src_path))
+
+                # os.remove(_src_path)
+
+                shutil.move(_src_path, _dst_path)
+                cv2.imshow(_src_file_id, img)
+
+                free_space = get_free_space_mb(src_path)
+                print('free_space {}'.format(free_space))
+
+            del_images = []
+            for existing_image in existing_images.keys():
+                if existing_image not in _src_files:
+                    cv2.destroyWindow(existing_image)
+                    del_images.append(existing_image)
+
+            for del_image in del_images:
+                del existing_images[del_image]
+
+        k = cv2.waitKey(1 - _pause)
+        if k == 27:
+            break
+        elif k == 32:
+            _pause = 1 - _pause
+
+    for existing_image in existing_images.keys():
+        cv2.destroyWindow(existing_image)
+
+
+if __name__ == '__main__':
+    main()
