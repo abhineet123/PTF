303a4784415771887079b44babac621b354cb823
abhineet123 <asingh1@ualberta.ca>
2020-06-23 11:47:57 -0600

    minor fix

--- runParallelInTmux.py
+++ runParallelInTmux.py
@@ -22,24 +22,30 @@ lines = open(in_fname, 'r').readlines()
 pane_to_commands = {}
 # pprint(lines)
 cmd_id = 0
+pane_id = ''
 for line in lines:
     _line = line.strip()
     if not _line:
         continue
+
+    if server and pane_id and not pane_id.startswith(server):
+        print('skipping {} with invalid server'.format(pane_id))
+        continue
+
     if _line.startswith('## @ '):
         pane_id = _line.replace('## @ ', '')
 
         if pane_id not in pane_to_commands:
             cmd_id += 1
-            if server and not pane_id.startswith(server):
-                print('skipping {} with invalid server'.format(pane_id))
-                continue
+
             if cmd_id < start_id:
                 print('skipping {} with too small cmd_id'.format(pane_id))
                 continue
+
             if cmd_id > end_id > start_id:
                 print('skipping {} with too large cmd_id'.format(pane_id))
                 break
+
             pane_to_commands[pane_id] = 'tmux send-keys -t {}'.format(pane_id)
         continue
     elif _line.startswith('# '):
