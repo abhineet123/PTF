4ab0240212887e70af4873d1f9bae8440100ec45
abhineet123 <asingh1@ualberta.ca>
2020-07-01 09:50:33 -0600

    minor fix

--- scripts/scroll.cmd
+++ scripts/scroll.cmd
@@ -1,2 +1,2 @@
 @echo off
-python3 "%~dp0\..\infinite_scroll.py" max_t=%1
+python3 "%~dp0\..\infinite_scroll_linux.py" max_t=%1
---
+++ scripts/scroll_win.cmd
@@ -0,0 +1,2 @@
+@echo off
+python3 "%~dp0\..\infinite_scroll.py" max_t=%1
