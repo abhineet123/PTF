3033f1c22e78c82812a3467a32fff09697417921
abhineet123 <asingh1@ualberta.ca>
2020-06-30 18:46:38 -0600

    minor fix

--- runParallelInTmux.py
+++ runParallelInTmux.py
@@ -37,66 +37,64 @@ def main():
         in_fname_no_ext, in_fname_ext = os.path.splitext(os.path.basename(in_fname))
         if in_fname_ext == '.bshm':
             in_fnames = open(in_fname, 'r').readlines()
-            lines = []
-            for __in_fname in in_fnames:
-                __in_fname = __in_fname.strip()
-                if not __in_fname:
-                    continue
-                print('Reading from: {}'.format(__in_fname))
-                lines += open(__in_fname, 'r').readlines()
+            in_fnames = [__in_fname.strip() for __in_fname in in_fnames if __in_fname.strip()]
         else:
+            in_fnames = [in_fname, ]
+
+        for in_fname in in_fnames:
+            print('Reading from: {}'.format(in_fname))
             lines = open(in_fname, 'r').readlines()
 
-        print('lines: {}'.format(pformat(lines)))
+            print('lines: {}'.format(pformat(lines)))
 
-        pane_to_commands = {}
-        # pprint(lines)
-        cmd_id = 0
-        pane_id = ''
-        for line in lines:
-            _line = line.strip()
-            if not _line:
-                continue
+            pane_to_commands = {}
+            # pprint(lines)
+            cmd_id = 0
+            pane_id = ''
+            for line in lines:
+                _line = line.strip()
+                if not _line:
+                    continue
 
-            if _line.startswith('## @ '):
-                pane_id = _line.replace('## @ ', '')
+                if _line.startswith('## @ '):
+                    pane_id = _line.replace('## @ ', '')
 
-                if pane_id not in pane_to_commands:
-                    cmd_id += 1
+                    if pane_id not in pane_to_commands:
+                        cmd_id += 1
 
-                    if cmd_id < start_id:
-                        print('skipping {} with too small cmd_id'.format(pane_id))
-                        continue
+                        if cmd_id < start_id:
+                            print('skipping {} with too small cmd_id'.format(pane_id))
+                            continue
 
-                    if cmd_id > end_id > start_id:
-                        print('skipping {} with too large cmd_id'.format(pane_id))
-                        break
+                        if cmd_id > end_id > start_id:
+                            print('skipping {} with too large cmd_id'.format(pane_id))
+                            break
 
-                    pane_to_commands[pane_id] = 'tmux send-keys -t {}'.format(pane_id)
-                continue
-            elif _line.startswith('# '):
-                continue
+                        pane_to_commands[pane_id] = 'tmux send-keys -t {}'.format(pane_id)
+                    continue
+                elif _line.startswith('# '):
+                    continue
 
-            if server and pane_id and not pane_id.startswith(server):
-                # print('skipping {} with invalid server'.format(pane_id))
-                if pane_id in pane_to_commands:
-                    del pane_to_commands[pane_id]
-                continue
+                if server and pane_id and not pane_id.startswith(server):
+                    # print('skipping {} with invalid server'.format(pane_id))
+                    if pane_id in pane_to_commands:
+                        del pane_to_commands[pane_id]
+                    continue
 
-            pane_to_commands[pane_id] = '{} "{}" Enter'.format(pane_to_commands[pane_id], _line)
+                pane_to_commands[pane_id] = '{} "{}" Enter'.format(pane_to_commands[pane_id], _line)
 
-        print('pane_to_commands: {}'.format(pformat(pane_to_commands)))
+            print('pane_to_commands: {}'.format(pformat(pane_to_commands)))
 
-        for pane_id in pane_to_commands:
-            print('running command in {}'.format(pane_id))
-            # print('running: {}'.format(pane_to_commands[pane_id]))
-            # os.system(pane_to_commands[pane_id])
+            for pane_id in pane_to_commands:
+                print('running command in {}'.format(pane_id))
+                # print('running: {}'.format(pane_to_commands[pane_id]))
+                # os.system(pane_to_commands[pane_id])
 
-            # cmd_prefix = 'tmux send-keys -t {}'.format(pane_id)
-            # for _line in pane_to_commands[pane_id]:
-            #     cmd = '{} "{}" Enter'.format(cmd_prefix, _line)
-            #     print('running: {}'.format(cmd))
-            #     os.system(cmd)
+                # cmd_prefix = 'tmux send-keys -t {}'.format(pane_id)
+                # for _line in pane_to_commands[pane_id]:
+                #     cmd = '{} "{}" Enter'.format(cmd_prefix, _line)
+                #     print('running: {}'.format(cmd))
+                #     os.system(cmd)
 
 
 if __name__ == '__main__':
