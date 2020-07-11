8266752e24f9c34e948fe806ed948568f3f8937d
abhineet123 <asingh1@ualberta.ca>
2020-07-11 09:05:43 -0600

    8:58:18 AM added support for directly specifying the single command to run in RPIT 9:05:17 AM Fixes some more therein to allow specifying the pane ID as well

--- runParallelInTmux.py
+++ runParallelInTmux.py
@@ -19,6 +19,8 @@ def main():
         'start_id': 0,
         'end_id': -1,
         'server': '',
+        'pane_id': '2.0',
+        'pane_id_sep': '>',
     }
     paramparse.process_dict(params)
 
@@ -28,7 +30,11 @@ def main():
     start_id = params['start_id']
     end_id = params['end_id']
     server = params['server']
+    pane_id_sep = params['pane_id_sep']
+    pane_id = params['pane_id']
+
     while True:
+        lines = None
         if _in_fname:
             in_fname = _in_fname
             _in_fname = ''
@@ -36,10 +42,18 @@ def main():
             in_fname = input('\nEnter script path\n')
 
         in_fname_no_ext, in_fname_ext = os.path.splitext(os.path.basename(in_fname))
-        if in_fname_ext == '.bshm':
+        if in_fname_ext == '.bsh':
+            in_fnames = [in_fname, ]
+        elif in_fname_ext == '.bshm':
             in_fnames = open(in_fname, 'r').readlines()
             in_fnames = [__in_fname.strip() for __in_fname in in_fnames if __in_fname.strip()]
         else:
+            if pane_id_sep in in_fname:
+                _pane_id, _line = in_fname.split(pane_id_sep)
+            else:
+                _pane_id = pane_id
+                _line = in_fname
+            lines = ['## @{}.{}'.format(server, _pane_id), _line]
             in_fnames = [in_fname, ]
 
         src_dir = os.getcwd()
@@ -48,14 +62,14 @@ def main():
         fname_to_path = dict([item for sublist in src_file_gen for item in sublist])
 
         for in_fname in in_fnames:
+            if lines is None:
+                try:
+                    in_fname_path = fname_to_path[in_fname]
+                except KeyError:
+                    raise IOError('invalid file name: {}'.format(in_fname))
 
-            try:
-                in_fname_path = fname_to_path[in_fname]
-            except KeyError:
-                raise IOError('invalid file name: {}'.format(in_fname))
-
-            print('\nReading from: {}'.format(in_fname_path))
-            lines = open(in_fname_path, 'r').readlines()
+                print('\nReading from: {}'.format(in_fname_path))
+                lines = open(in_fname_path, 'r').readlines()
 
             # print('lines: {}'.format(pformat(lines)))
 
