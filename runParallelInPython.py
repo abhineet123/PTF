1b0a916397acf0ea36a2ab632f7a80e5e1075446
abhineet123 <asingh1@ualberta.ca>
2021-06-06 13:54:26 -0600

    minor fix

--- runParallelInPython.py
+++ runParallelInPython.py
@@ -13,6 +13,7 @@ import paramparse
 def linux_path(*args, **kwargs):
     return os.path.join(*args, **kwargs).replace(os.sep, '/')
 
+
 def write(_str):
     with open('rpip.ansi', 'a') as fid:
         fid.write(_str + '\n')
@@ -35,13 +36,11 @@ def main():
 
     # processArguments(sys.argv[1:], params)
 
-    in_fname = params['in_fname']
+    _in_fname = params['in_fname']
     working_dir = params['working_dir']
     server = params['server']
     log_dir = params['log_dir']
     enable_logging = params['enable_logging']
-
-    lines = None
 
     if working_dir:
         os.chdir(working_dir)
@@ -53,129 +52,138 @@ def main():
                     for (dirpath, dirnames, filenames) in os.walk(src_dir, followlinks=True)]
     fname_to_path = dict([item for sublist in src_file_gen for item in sublist])
 
-    write('\nprocessing input: {}'.format(in_fname))
+    while True:
+        lines = None
+        if _in_fname:
+            in_fname = _in_fname
+            _in_fname = ''
+        else:
+            in_fname = input('\nEnter script path or command\n')
 
-    in_fname_no_ext, in_fname_ext = os.path.splitext(os.path.basename(in_fname))
-    if in_fname_ext in ('.bsh', '.md'):
-        in_fnames = [in_fname, ]
-    elif in_fname_ext == '.bshm':
-        try:
-            in_fname_path = fname_to_path[in_fname]
-            # in_fname_path = in_fname
-        except KeyError:
-            raise IOError('invalid file name: {}'.format(in_fname))
+        write('\nprocessing input: {}'.format(in_fname))
 
-        in_fnames = open(in_fname_path, 'r').readlines()
-        in_fnames = [__in_fname.strip() for __in_fname in in_fnames if __in_fname.strip()
-                     and not __in_fname.startswith('#')]
-
-        # write('lines:\n{}'.format(lines))
-    commands = []
-
-    time_stamp = datetime.now().strftime("%y%m%d_%H%M%S")
-
-    for in_fname in in_fnames:
-        if in_fname is not None:
+        in_fname_no_ext, in_fname_ext = os.path.splitext(os.path.basename(in_fname))
+        if in_fname_ext in ('.bsh', '.md'):
+            in_fnames = [in_fname, ]
+        elif in_fname_ext == '.bshm':
             try:
                 in_fname_path = fname_to_path[in_fname]
                 # in_fname_path = in_fname
             except KeyError:
                 raise IOError('invalid file name: {}'.format(in_fname))
 
-            write('\nReading from: {}'.format(in_fname_path))
-            lines = open(in_fname_path, 'r').readlines()
-        else:
-            assert lines is not None, "Both lines and in_fname cannot be None"
+            in_fnames = open(in_fname_path, 'r').readlines()
+            in_fnames = [__in_fname.strip() for __in_fname in in_fnames if __in_fname.strip()
+                         and not __in_fname.startswith('#')]
 
-        basename = os.path.basename(in_fname_path)
-        basename_no_ext, _ = os.path.splitext(basename)
+            # write('lines:\n{}'.format(lines))
+        commands = []
 
-        # write('lines: {}'.format(pformat(lines)))
+        time_stamp = datetime.now().strftime("%y%m%d_%H%M%S")
 
-        # command_lines = [_line for _line in lines if not _line.startswith('## @ ') and not _line.startswith('# ')]
+        for in_fname in in_fnames:
+            if in_fname is not None:
+                try:
+                    in_fname_path = fname_to_path[in_fname]
+                    # in_fname_path = in_fname
+                except KeyError:
+                    raise IOError('invalid file name: {}'.format(in_fname))
 
-        valid_line_id = 0
-        for line in lines:
-            _line = line.strip()
-            if not _line or not _line.startswith('python'):
-                continue
+                write('\nReading from: {}'.format(in_fname_path))
+                lines = open(in_fname_path, 'r').readlines()
+            else:
+                assert lines is not None, "Both lines and in_fname cannot be None"
 
-            list_start_indices = [m.start() for m in re.finditer("{", _line)]
-            list_end_indices = [m.start() for m in re.finditer("}", _line)]
+            basename = os.path.basename(in_fname_path)
+            basename_no_ext, _ = os.path.splitext(basename)
 
-            assert len(list_start_indices) == len(list_end_indices), \
-                "mismatch between number of list start and end markers"
+            # write('lines: {}'.format(pformat(lines)))
 
-            _multi_token_lines = [_line, ]
-            for _start_id, _end_id in zip(list_start_indices, list_end_indices):
-                substr = _line[_start_id:_end_id + 1]
+            # command_lines = [_line for _line in lines if not _line.startswith('## @ ') and not _line.startswith('# ')]
 
-                replacement_vals = paramparse.str_to_tuple(substr[1:-1])
+            valid_line_id = 0
+            for line in lines:
+                _line = line.strip()
+                if not _line or not _line.startswith('python'):
+                    continue
 
-                temp_line_list = []
-                for __line in _multi_token_lines:
-                    for _val in replacement_vals:
-                        new_line = __line.replace(substr, str(_val), 1)
-                        temp_line_list.append(new_line)
+                list_start_indices = [m.start() for m in re.finditer("{", _line)]
+                list_end_indices = [m.start() for m in re.finditer("}", _line)]
 
-                _multi_token_lines = temp_line_list
+                assert len(list_start_indices) == len(list_end_indices), \
+                    "mismatch between number of list start and end markers"
 
-            for __line_id, __line in enumerate(_multi_token_lines):
-                tee_log_id = '{}_{}_{}_{}'.format(basename_no_ext, valid_line_id, __line_id, time_stamp)
-                if server:
-                    tee_log_id = '{}_{}'.format(tee_log_id, server)
+                _multi_token_lines = [_line, ]
+                for _start_id, _end_id in zip(list_start_indices, list_end_indices):
+                    substr = _line[_start_id:_end_id + 1]
 
-                if enable_logging:
-                    __line = '{} @ tee_log={}'.format(__line, tee_log_id)
+                    replacement_vals = paramparse.str_to_tuple(substr[1:-1])
 
-                    """disable python output buffering to ensure in-order output in the logging fine"""
-                    if __line.startswith('python '):
-                        __line = __line.replace('python ', 'python -u ', 1)
-                    elif __line.startswith('python3 '):
-                        __line = __line.replace('python3 ', 'python3 -u ', 1)
-                    elif __line.startswith('python36 '):
-                        __line = __line.replace('python36 ', 'python36 -u ', 1)
-                    elif __line.startswith('python2 '):
-                        __line = __line.replace('python2 ', 'python2 -u ', 1)
+                    temp_line_list = []
+                    for __line in _multi_token_lines:
+                        for _val in replacement_vals:
+                            new_line = __line.replace(substr, str(_val), 1)
+                            temp_line_list.append(new_line)
 
-                commands.append((__line, tee_log_id))
-            valid_line_id += 1
+                    _multi_token_lines = temp_line_list
 
-    processes = []
-    for _cmd_id, _cmd_data in enumerate(commands):
-        _cmd, tee_log_id = _cmd_data
-        txt = 'running command {}: {}'.format(_cmd_id, _cmd)
-        write(txt)
-        # subprocess.Popen(_cmd.split(' '))
+                for __line_id, __line in enumerate(_multi_token_lines):
+                    tee_log_id = '{}_{}_{}_{}'.format(basename_no_ext, valid_line_id, __line_id, time_stamp)
+                    if server:
+                        tee_log_id = '{}_{}'.format(tee_log_id, server)
 
-        args = shlex.split(_cmd)
+                    if enable_logging:
+                        __line = '{} @ tee_log={}'.format(__line, tee_log_id)
 
-        if enable_logging:
-            out_fname = tee_log_id + '.ansi'
-            zip_fname = out_fname.replace('.ansi', '.zip')
+                        """disable python output buffering to ensure in-order output in the logging fine"""
+                        if __line.startswith('python '):
+                            __line = __line.replace('python ', 'python -u ', 1)
+                        elif __line.startswith('python3 '):
+                            __line = __line.replace('python3 ', 'python3 -u ', 1)
+                        elif __line.startswith('python36 '):
+                            __line = __line.replace('python36 ', 'python36 -u ', 1)
+                        elif __line.startswith('python2 '):
+                            __line = __line.replace('python2 ', 'python2 -u ', 1)
 
-            out_path = linux_path(log_dir, out_fname)
-            zip_path = os.path.join(log_dir, zip_fname)
+                    commands.append((__line, tee_log_id))
+                valid_line_id += 1
 
-            write('{}\n'.format(zip_path))
-            
-            f = open(out_path, 'w')
-            p = subprocess.Popen(args, stdout=f, stderr=f)
-        else:
-            write('\n')
-            f = out_fname = zip_fname = None
-            p = subprocess.Popen(args)
+        processes = []
+        for _cmd_id, _cmd_data in enumerate(commands):
+            _cmd, tee_log_id = _cmd_data
+            txt = 'running command {}: {}'.format(_cmd_id, _cmd)
+            write(txt)
+            # subprocess.Popen(_cmd.split(' '))
 
-        processes.append((p, f, out_fname, zip_fname))
+            args = shlex.split(_cmd)
 
-    for p, f, f_name, zip_fname in processes:
-        p.wait()
-        if f is not None:
-            f.close()
+            if enable_logging:
+                out_fname = tee_log_id + '.ansi'
+                zip_fname = out_fname.replace('.ansi', '.zip')
 
-        zip_cmd = 'cd {} && zip -rm {} {}'.format(log_dir, zip_fname, f_name)
+                out_path = linux_path(log_dir, out_fname)
+                zip_path = os.path.join(log_dir, zip_fname)
 
-        os.system(zip_cmd)
+                write('{}\n'.format(zip_path))
+
+                f = open(out_path, 'w')
+                p = subprocess.Popen(args, stdout=f, stderr=f)
+            else:
+                write('\n')
+                f = out_fname = zip_fname = None
+                p = subprocess.Popen(args)
+
+            processes.append((p, f, out_fname, zip_fname))
+
+        for p, f, f_name, zip_fname in processes:
+            p.wait()
+            if f is not None:
+                f.close()
+
+            zip_cmd = 'cd {} && zip -rm {} {}'.format(log_dir, zip_fname, f_name)
+
+            os.system(zip_cmd)
+
 
 if __name__ == '__main__':
     main()
