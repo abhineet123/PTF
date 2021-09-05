01d4aee34c313088d61feb33c06d27c118516a9f
abhineet123 <asingh1@ualberta.ca>
2021-09-04 20:45:38 -0600

    Finally apparently got the automatic link adding in the categorized entry processing script thingy working

--- processTimingData.py
+++ processTimingData.py
@@ -1,13 +1,8 @@
 from datetime import datetime, timedelta
 
-try:
-    from Tkinter import Tk
-except ImportError:
-    from tkinter import Tk
-    # import tkinter as Tk
-
 import time
 import win32gui, win32api
+import win32clipboard
 from pywinauto import application, mouse
 import os
 import shutil
@@ -33,6 +28,8 @@ class Params:
         self.txt_proc_list = 'processed.log'
         self.recursive = 1
         self.category = 2
+        self.paste_cmd = ''
+        self.link_cmd = ''
         self.ffs = 1
         self.ffs_exe = ''
         self.ffs_root = ''
@@ -112,173 +109,226 @@ def process(in_txt, verbose=1):
     return out_txt
 
 
-def copy_to_clipboard(out_txt):
+def copy_from_clipboard():
     try:
+        win32clipboard.OpenClipboard()
+        in_txt = win32clipboard.GetClipboardData()
+    except BaseException as e:
+        print('GetClipboardData failed: {}'.format(e))
+        return None
+
+    win32clipboard.CloseClipboard()
+    return in_txt
+
+
+def copy_to_clipboard(out_txt, print_txt=0):
+    if print_txt:
+        print(out_txt)
+    try:
+        # win32clipboard.OpenClipboard()
+        # win32clipboard.SetClipboardText(out_txt)
         import pyperclip
 
         pyperclip.copy(out_txt)
         spam = pyperclip.paste()
     except BaseException as e:
         print('Copying to clipboard failed: {}'.format(e))
+        return
+
+    # win32clipboard.CloseClipboard()
 
 
 def main():
     params = Params()
     paramparse.process(params)
 
-    try:
-        in_txt = Tk().clipboard_get()
-    except BaseException as e:
-        print('Tk().clipboard_get() failed: {}'.format(e))
+    in_txt = copy_from_clipboard()
+    if in_txt is None:
         lines = []
     else:
         lines = in_txt.split('\n')
-        lines = [line for line in lines if line.strip()]
-
-    out_txt = ''
+        lines = [line.strip() for line in lines if line.strip()]
 
     if lines:
-        if all(line.endswith('.ogg') for line in lines):
-            out_lines = [line.split('-')[1].split('.')[0].replace('_', ':') for line in lines]
+        is_path = all(line.startswith('"') and line.endswith('"') for line in lines)
+        if is_path:
+            stripped_lines = [line.strip('"') for line in lines]
+        else:
+            stripped_lines = lines[:]
+
+        is_ogg = all(line.endswith('.ogg') for line in stripped_lines)
+
+        print('is_ogg: {}'.format(is_ogg))
+        print('stripped_lines: {}'.format(stripped_lines))
+        print('lines: {}'.format(lines))
+        #
+        # input('press any key')
+
+        if is_ogg:
+            base_names = [os.path.basename(line) for line in stripped_lines]
+            out_lines = [base_name.split('-')[1].split('.')[0].replace('_', ':') for base_name in base_names]
             if params.category > 0:
                 out_lines = ['{} :: {}'.format(line, params.category) for line in out_lines]
 
-            out_txt = '\n'.join(sorted(out_lines))
+            sort_idx = sorted(range(len(out_lines)), key=out_lines.__getitem__)
 
+            out_lines = [out_lines[k] for k in sort_idx]
+            lines = [lines[k] for k in sort_idx]
+
+            out_txt = '\n'.join(out_lines)
+            copy_to_clipboard(out_txt, print_txt=1)
+
+            in_txt = '\n'.join(lines)
+            print(in_txt)
+            input('press any key')
+
+            print('is_path: {}'.format(is_path))
+            print('paste_cmd: {}'.format(params.paste_cmd))
+            print('link_cmd: {}'.format(params.link_cmd))
+
+            if is_path and params.paste_cmd and params.link_cmd:
+                os.system(params.paste_cmd)
+                for _path in lines[::-1]:
+                    print(_path)
+                    copy_to_clipboard(_path, print_txt=1)
+                    # input('press any key')
+                    os.system(params.link_cmd)
+                    time.sleep(1)
+            return
         else:
             try:
                 out_txt = process(in_txt)
             except:
-                out_txt = ''
-
-    if not out_txt:
-
-        if params.ffs:
-            for _ffs_file in params.ffs_files:
-                ffs_path = os.path.join(params.ffs_root, _ffs_file + '.' + params.ffs_ext)
-                ffs_cmd = '{} "{}"'.format(params.ffs_exe, ffs_path)
-                print(ffs_cmd)
-                os.system(ffs_cmd)
-
-        if params.txt_path:
-
-            assert os.path.isdir(params.txt_path), "embedded text path: {}".format(params.txt_path)
-
-            if params.recursive:
-                files_gen = [[linux_path(dirpath, f) for f in filenames if
-                              f.endswith('.txt') and f.startswith('Timing')]
-                             for (dirpath, dirnames, filenames) in os.walk(params.txt_path, followlinks=True)]
-                files = [item for sublist in files_gen for item in sublist]
+                pass
             else:
-                files = os.listdir(params.txt_path)
-                files = [linux_path(params.txt_path, k) for k in files if k and k.endswith('.txt')]
+                copy_to_clipboard(out_txt, print_txt=1)
+                return
 
-            txt_proc_list_path = linux_path(params.txt_path, params.txt_proc_list)
+    if params.ffs:
+        for _ffs_file in params.ffs_files:
+            ffs_path = os.path.join(params.ffs_root, _ffs_file + '.' + params.ffs_ext)
+            ffs_cmd = '{} "{}"'.format(params.ffs_exe, ffs_path)
+            print(ffs_cmd)
+            os.system(ffs_cmd)
 
-            if os.path.isfile(txt_proc_list_path):
-                processed_files = open(txt_proc_list_path, 'r').readlines()
-                processed_files = [k.strip().split('\t')[1] for k in processed_files if k.strip()]
+    if params.txt_path:
 
-                files = [k for k in files if k not in processed_files]
+        assert os.path.isdir(params.txt_path), "embedded text path: {}".format(params.txt_path)
 
-            files.sort(key=os.path.getmtime)
-            n_files = len(files)
+        if params.recursive:
+            files_gen = [[linux_path(dirpath, f) for f in filenames if
+                          f.endswith('.txt') and f.startswith('Timing')]
+                         for (dirpath, dirnames, filenames) in os.walk(params.txt_path, followlinks=True)]
+            files = [item for sublist in files_gen for item in sublist]
+        else:
+            files = os.listdir(params.txt_path)
+            files = [linux_path(params.txt_path, k) for k in files if k and k.endswith('.txt')]
 
-            if n_files > 1:
-                print('found {} new files:\n{}'.format(n_files, '\n'.join(files)))
+        txt_proc_list_path = linux_path(params.txt_path, params.txt_proc_list)
 
-            for file_id, file in enumerate(files):
-                print('reading file {} / {}: {}'.format(file_id + 1, n_files, file))
+        if os.path.isfile(txt_proc_list_path):
+            processed_files = open(txt_proc_list_path, 'r').readlines()
+            processed_files = [k.strip().split('\t')[1] for k in processed_files if k.strip()]
 
-                # file = dst_file
+            files = [k for k in files if k not in processed_files]
 
-                in_txt = open(file, 'r').read()
-                out_txt = process(in_txt, verbose=0)
-                print(out_txt)
+        files.sort(key=os.path.getmtime)
+        n_files = len(files)
 
-                copy_to_clipboard(out_txt)
+        if n_files > 1:
+            print('found {} new files:\n{}'.format(n_files, '\n'.join(files)))
 
-                with open(txt_proc_list_path, 'r+') as f:
-                    content = f.read()
-                    f.seek(0, 0)
+        for file_id, file in enumerate(files):
+            print('reading file {} / {}: {}'.format(file_id + 1, n_files, file))
 
-                    timestamp_str = datetime.now().strftime("%y%m%d %H:%M:%S.%f")[:-4]
+            # file = dst_file
 
-                    txt = '{}\t{}\n'.format(timestamp_str, file)
-                    f.write(txt + content)
+            in_txt = open(file, 'r').read()
+            out_txt = process(in_txt, verbose=0)
+            print(out_txt)
 
-                # dst_file = file.replace('.txt', '.log')
-                # shutil.move(file, dst_file)
-                os.system("vscode {}".format(file))
+            copy_to_clipboard(out_txt)
 
-                _ = input('\npress any key to continue\n')
+            with open(txt_proc_list_path, 'r+') as f:
+                content = f.read()
+                f.seek(0, 0)
 
-            return
+                timestamp_str = datetime.now().strftime("%y%m%d %H:%M:%S.%f")[:-4]
 
-        # time.sleep(1)
+                txt = '{}\t{}\n'.format(timestamp_str, file)
+                f.write(txt + content)
+
+            # dst_file = file.replace('.txt', '.log')
+            # shutil.move(file, dst_file)
+            os.system("vscode {}".format(file))
+
+            _ = input('\npress any key to continue\n')
+
+        return
+
+    # time.sleep(1)
+    try:
+        orig_x, orig_y = win32api.GetCursorPos()
+        print('GetCursorPos x: {}'.format(orig_x))
+        print('GetCursorPos y: {}'.format(orig_y))
+
+        win32gui.EnumWindows(foreach_window, None)
+
+        # for i in range(len(titles)):
+        #     print(titles[i])
+
+        target_title = [k[1] for k in titles if all(title in k[1] for title in params.win_titles)]
+        # print('target_title: {}'.format(target_title))
+
+        if not target_title:
+            raise IOError('Window with win_titles: {} not found'.format(params.win_titles))
+
+        target_title = target_title[0]
+
+        target_handle = win32gui.FindWindow(None, target_title)
+        rect = win32gui.GetWindowRect(target_handle)
+
+        x = int((rect[0] + rect[2]) / 2)
+        y = int((rect[1] + rect[3]) / 2)
+
+        # active_handle = win32gui.GetForegroundWindow()
+        # target_title = win32gui.GetWindowText(active_handle)
+
+        print('target_title: {}'.format(target_title))
+        print('rect: {}'.format(rect))
+        print('x: {}'.format(x))
+        print('y: {}'.format(y))
+
         try:
-            orig_x, orig_y = win32api.GetCursorPos()
-            print('GetCursorPos x: {}'.format(orig_x))
-            print('GetCursorPos y: {}'.format(orig_y))
-
-            win32gui.EnumWindows(foreach_window, None)
-
-            # for i in range(len(titles)):
-            #     print(titles[i])
-
-            target_title = [k[1] for k in titles if all(title in k[1] for title in params.win_titles)]
-            # print('target_title: {}'.format(target_title))
-
-            if not target_title:
-                raise IOError('Window with win_titles: {} not found'.format(params.win_titles))
-
-            target_title = target_title[0]
-
-            target_handle = win32gui.FindWindow(None, target_title)
-            rect = win32gui.GetWindowRect(target_handle)
-
-            x = int((rect[0] + rect[2]) / 2)
-            y = int((rect[1] + rect[3]) / 2)
-
-            # active_handle = win32gui.GetForegroundWindow()
-            # target_title = win32gui.GetWindowText(active_handle)
-
-            print('target_title: {}'.format(target_title))
-            print('rect: {}'.format(rect))
-            print('x: {}'.format(x))
-            print('y: {}'.format(y))
-
-            try:
-                app = application.Application().connect(title=target_title, found_index=0)
-            except BaseException as e:
-                print('Failed to connect to app for window {}: {}'.format(target_title, e))
-                exit(0)
-            try:
-                app_win = app.window(title=target_title)
-            except BaseException as e:
-                print('Failed to access app window for {}: {}'.format(target_title, e))
-                exit(0)
-            app_win.type_keys("^a")
-            app_win.type_keys("^c")
-
-            mouse.move(coords=(x, y))
-
-            mouse.click(button='left', coords=(x, y))
-            mouse.click(button='left', coords=(x, y))
-
-            mouse.move(coords=(orig_x, orig_y))
-
+            app = application.Application().connect(title=target_title, found_index=0)
         except BaseException as e:
-            print('BaseException: {}'.format(e))
+            print('Failed to connect to app for window {}: {}'.format(target_title, e))
+            exit(0)
+        try:
+            app_win = app.window(title=target_title)
+        except BaseException as e:
+            print('Failed to access app window for {}: {}'.format(target_title, e))
+            exit(0)
+        app_win.type_keys("^a")
+        app_win.type_keys("^c")
 
-        in_txt = Tk().clipboard_get()
-        out_txt = process(in_txt)
+        mouse.move(coords=(x, y))
 
-    print(out_txt)
+        mouse.click(button='left', coords=(x, y))
+        mouse.click(button='left', coords=(x, y))
+
+        mouse.move(coords=(orig_x, orig_y))
+
+    except BaseException as e:
+        print('BaseException: {}'.format(e))
+
+    in_txt = copy_from_clipboard()
+    out_txt = process(in_txt)
+
     # with open(out_fname, 'w') as out_fid:
     #     out_fid.write(out_txt)
 
-    copy_to_clipboard(out_txt)
+    copy_to_clipboard(out_txt, print_txt=1)
 
 
 if __name__ == '__main__':
