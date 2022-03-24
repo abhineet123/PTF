1aa95ad14ebade90c2db3f8e82ba164f7bc87475
abhineet123 <asingh1@ualberta.ca>
2022-03-24 21:25:09 +0100

    minor fix

--- moveFilesToSubDirs.py
+++ moveFilesToSubDirs.py
@@ -31,6 +31,9 @@ src_file_names = [f for f in os.listdir(root_dir) if os.path.isfile(os.path.join
 if shuffle_files:
     print('Shuffling files...')
     random.shuffle(src_file_names)
+    
+src_file_names.sort()
+
 
 subdir_id = subdir_start_id
 file_count = 0
@@ -38,41 +41,47 @@ dir_file_id = 1
 
 n_files = len(src_file_names)
 
+if subdir_name_from_files:
+    subdir_prefix = 'temp'
+
+
 first_file_name = None
 for src_id, src_fname in enumerate(src_file_names):
-    filename, file_extension = os.path.splitext(src_fname)
+    src_fname_noext, src_ext = os.path.splitext(src_fname)
     src_path = os.path.join(root_dir, src_fname)
 
-    if subdir_name_from_files or subdir_prefix:
+    if subdir_prefix:
         if rename_files:
-            dst_fname = '{:s}{:d}_{:d}.{:s}'.format(subdir_prefix, subdir_id, dir_file_id, file_extension)
+            dst_fname = '{:s}{:d}_{:d}.{:s}'.format(subdir_prefix, subdir_id, dir_file_id, src_ext)
         else:
             dst_fname = src_fname
         dst_subdir_name = '{:s}{:d}'.format(subdir_prefix, subdir_id)
     else:
+        """move each file to its own subdirectory"""
         dst_fname = src_fname
-        dst_subdir_name = os.path.splitext(src_fname)[0]
+        dst_subdir_name = src_fname_noext
 
     dst_dir = os.path.join(root_dir, dst_subdir_name)
-    if not os.path.exists(dst_dir):
-        os.makedirs(dst_dir)
+    os.makedirs(dst_dir, exist_ok=1)
 
     dst_fname_no_ext = os.path.splitext(dst_fname)[0]
     if first_file_name is None:
         first_file_name = dst_fname_no_ext
 
     dst_path = os.path.join(dst_dir, dst_fname)
-    shutil.move(src_path, dst_path)
+
+    print('{} --> {}'.format(src_path, dst_path))
+    # shutil.move(src_path, dst_path)
+
     if dir_file_id == files_per_subdir or src_id == n_files - 1:
-        print('Done sub dir {:d}'.format(subdir_id))
+        # print('Done sub dir {:d}'.format(subdir_id))
 
         if subdir_name_from_files:
             final_dst_subdir_name = '{}_to_{}'.format(first_file_name, dst_fname_no_ext)
-            print('changing name to {}'.format(final_dst_subdir_name))
-
             final_dst_dir = os.path.join(root_dir, final_dst_subdir_name)
 
-            shutil.move(dst_dir, final_dst_dir)
+            print('\n{} --> {}\n'.format(dst_dir, final_dst_dir))
+            # shutil.move(dst_dir, final_dst_dir)
 
         subdir_id += 1
         first_file_name = None
