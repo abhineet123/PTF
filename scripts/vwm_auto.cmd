e0fe34d97ad191547a8f8b39648a0a286ffcf44c
abhineet123 <asingh1@ualberta.ca>
2022-09-20 17:20:13 +0100

    minor fix

---
+++ scripts/vwm_auto.cmd
@@ -0,0 +1,3 @@
+@echo off
+python3 "%~dp0\..\visualizeWithMotion.py" src_path=%1 n_images=%2 lazy_video_load=0 fullscreen=1 exclude_src_dirs="masks,contours" trim_images=0 random_mode=1 auto_progress=1 transition_interval=15
+
