f972bb75ee6e049bc26cd6bb1ff84a5bb787b704
abhineet123 <asingh1@ualberta.ca>
2024-03-16 12:09:35 -0600

    minor fix

--- pomodoro.py
+++ pomodoro.py
@@ -1,32 +1,56 @@
-import os
-
 import paramparse
 import time
 from tqdm import tqdm
 from win10toast import ToastNotifier
+import winsound
 
 
 class Params:
-
-    def __init__(self):
-        self.cfg = ()
-        self.interval = 360
-        self.n_intervals = 1000
+    sound = ""
+    # sound = "Rhea.wav"
+    duration = 5
+    interval = 360
+    n_intervals = 1000
+    freq = 5000  # Hz
+    threaded = 0
 
 
 def main():
     params = Params()
     paramparse.process(params)
 
+    if params.sound:
+        winsound.PlaySound(params.sound, winsound.SND_FILENAME)
+
+    toaster = ToastNotifier()
+    toaster.show_toast(
+        title=f"Pomodoro interval timer",
+        msg=f"starting timer for {params.n_intervals} intervals of {params.interval} secs each",
+        icon_path="pomodoro.ico",
+        duration=3,
+        threaded=params.threaded,
+    )
+
     for i in range(params.n_intervals):
 
-        for _ in tqdm(range(params.interval), desc=f'interval {i + 1}'):
+        for _ in tqdm(range(params.interval), desc=f'interval {i + 1} / {params.n_intervals}', ncols=100):
             time.sleep(1)
 
         toaster = ToastNotifier()
-        toaster.show_toast(f"interval {i + 1} / {params.n_intervals} completed",
-                           icon_path="pomodoro.ico",
-                           duration=2)
+        if params.sound:
+            # winsound.Beep(params.freq, 1000)
+            # winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
+            winsound.PlaySound(params.sound, winsound.SND_FILENAME)
+
+        msg_ = f"interval {i + 1} / {params.n_intervals} completed"
+        msg = '\n'.join([msg_, ]*4)
+        toaster.show_toast(
+            title=f"Pomodoro interval timer",
+            msg=msg,
+            icon_path="pomodoro.ico",
+            duration=params.duration,
+            threaded=params.threaded,
+        )
 
 
 if __name__ == '__main__':
