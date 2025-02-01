4dfba18f60bd008e8f59920bbd97cbd7f8f1b87e
abhineet123 <asingh1@ualberta.ca>
2025-01-31 17:53:09 -0700

    minor fix

--- pdfCrop.py
+++ pdfCrop.py
@@ -11,9 +11,13 @@ class Params:
         self.folder_name = '.'
         self.live = 0
         self.recursive = 0
+        self.oxps = 0
 
 
-def run(params:Params, src_files=None):
+def run(params: Params, src_files=None):
+    if params.oxps:
+        params.file_ext = 'oxps'
+
     if src_files is None or not src_files[0]:
         if params.recursive:
             src_file_gen = [[os.path.join(dirpath, f) for f in filenames if
@@ -24,19 +28,23 @@ def run(params:Params, src_files=None):
             src_files = [os.path.join(params.folder_name, f) for f in os.listdir(params.folder_name)
                          if os.path.isfile(os.path.join(params.folder_name, f)) and f.endswith(params.file_ext)]
 
-        src_files = [k for k in src_files if not k.endswith('-crop.pdf')]
+        if not params.oxps:
+            src_files = [k for k in src_files if not k.endswith('-crop.pdf')]
 
         src_files.sort(key=sortKey)
 
     n_files = len(src_files)
 
     for i, _f in enumerate(src_files):
-        if not _f.endswith('.pdf'):
-            _f = f'{_f}.pdf'
-        cmd = f'pdfcrop {_f}'
+        __f = os.path.splitext(_f)[0]
+        if params.oxps:
+            cmd = f'gxpswin64 -sDEVICE=pdfwrite -sOutputFile={__f}.pdf -dNOPAUSE {__f}.oxps'
+            print(f'{i + 1}/{n_files} {cmd}')
+            os.system(cmd)
+
+        cmd = f'pdfcrop {__f}.pdf'
         print(f'{i + 1}/{n_files} {cmd}')
         os.system(cmd)
-
 
 def main():
     params: Params = paramparse.process(Params)
