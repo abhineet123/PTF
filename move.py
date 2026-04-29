907066cb8eee9f4833a12396199279d765870008
abhineet123 <asingh1@ualberta.ca>
2026-04-29 15:03:17 -0600

    add mv_from_list.py

--- move.py
+++ move.py
@@ -18,20 +18,20 @@ if len(sys.argv) > arg_id:
     root_dir = sys.argv[arg_id]
     arg_id += 1
 
-dst_path = '{:s}{:s}{:s}{:s}'.format(root_dir,os.sep, dst_dir, os.sep)
-print 'Searching for {:s} in {:s} to move to {:s}'.format(src_substr, root_dir, dst_dir)
+dst_path = '{:s}{:s}{:s}{:s}'.format(root_dir, os.sep, dst_dir, os.sep)
+print('Searching for {:s} in {:s} to move to {:s}'.format(src_substr, root_dir, dst_dir))
 if not os.path.exists(dst_path):
-    print 'Destination folder does not exist. Creating it...'
+    print('Destination folder does not exist. Creating it...')
     os.mkdir(dst_path)
 
 src_fnames = []
 for root, dirnames, filenames in os.walk(root_dir):
     for filename in fnmatch.filter(filenames, '*{:s}*'.format(src_substr)):
         src_fnames.append(os.path.join(root, filename))
-print 'Found {:d} matches'.format(len(src_fnames))
+print('Found {:d} matches'.format(len(src_fnames)))
 for src_fname in src_fnames:
     dst_fname = src_fname.replace('{:s}{:s}'.format(root_dir, os.sep), '{:s}{:s}{:s}{:s}'.format(
-        root_dir,os.sep, dst_dir, os.sep))
+        root_dir, os.sep, dst_dir, os.sep))
     splitlocaldir = dst_fname.split(os.sep)
     splitlocaldir.remove(splitlocaldir[-1:][0])
     localdir = ""
@@ -40,7 +40,7 @@ for src_fname in src_fnames:
     if not os.path.exists(localdir):
         os.makedirs(localdir)
     # dst_fname = '{:s}\{:s}'.format(root_dir, src_fname)
-    print 'moving {:s} to {:s}'.format(src_fname, dst_fname)
+    print('moving {:s} to {:s}'.format(src_fname, dst_fname))
     # shutil.move(src_fname, dst_fname)
     os.rename(src_fname, dst_fname)
     # print matches
---
+++ mv_from_list.py
@@ -0,0 +1,79 @@
+import os, sys, glob, re
+import time
+import paramparse
+from datetime import datetime
+
+from Misc import sortKey
+
+
+def get_size(start_path='.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+
+    return total_size
+
+
+class Params:
+    def __init__(self):
+        self.cfg = ()
+        self.dst_path = ''
+        self.file_name = ''
+        self.list_file = ''
+        self.root_dir = '.'
+
+
+if __name__ == '__main__':
+
+    params:Params = paramparse.process(Params)
+
+    list_file = params.list_file
+    root_dir = params.root_dir
+    file_name = params.file_name
+    dst_path = params.dst_path
+
+    if not dst_path or not os.path.isdir(dst_path):
+        raise IOError('dst_path is invalid: {}'.format(dst_path))
+
+    abs_dst_path = os.path.abspath(dst_path).replace(os.sep, '/')
+    print('abs_dst_path: {}'.format(abs_dst_path))
+    dst_drive = os.path.splitdrive(abs_dst_path)[0]
+    print('dst_drive: {}'.format(dst_drive))
+
+    time_stamp = datetime.now().strftime("%y%m%d_%H%M%S")
+    root_dir_name = ''
+
+    if os.path.isdir(list_file):
+        src_paths = [os.path.join(list_file, name) for name in os.listdir(list_file) if
+                     os.path.isdir(os.path.join(list_file, name))]
+        src_paths.sort(key=sortKey)
+    else:
+        src_paths = [x.strip() for x in open(list_file).readlines() if x.strip()
+                     and not x.startswith('#')
+                     and not x.startswith('@')]
+        if root_dir:
+            root_dir = os.path.abspath(root_dir)
+            root_dir_name = os.path.basename(root_dir)
+
+            src_paths = [os.path.join(root_dir, name) for name in src_paths]
+    out_file_path = '{}_{}'.format(list_file, time_stamp)
+
+    n_src_paths = len(src_paths)
+
+    speed = 0
+    start_t = time.time()
+    src_path_txt = ''
+    for i, src_path in enumerate(src_paths):
+        if src_path.startswith('#'):
+            continue
+        if not os.path.exists(src_path):
+            raise IOError('src_path does not exist: {}'.format(src_path))
+
+        mv_cmd = f'mv "{src_path}" "{dst_path}/"'
+
+        print(f'\n{i + 1}/{n_src_paths} :: running: {mv_cmd}')
+        # os.system(mv_cmd)
